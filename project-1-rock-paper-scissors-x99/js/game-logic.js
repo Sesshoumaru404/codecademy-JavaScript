d1ec8ac750680e167a0bb888f29cfaa5dd588e73
Paul <sesshoumaru404@outlook.com>
2017-10-05 16:36:23 -0400

    added global

--- project-1-rock-paper-scissors-x99/js/game-logic.js
+++ project-1-rock-paper-scissors-x99/js/game-logic.js
@@ -1,1 +1,33 @@
 // All code should be written in this file.
+//
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+const setPlayerMoves = (player, type1, value1, type2, value2, type3, value3) => {
+	if (player === 'Player One'){
+		playerOneMoveOneType = type1;
+		playerOneMoveTwoType = type2;
+		playerOneMoveThreeType = type3;
+		playerOneMoveOneValue = value1;
+		playerOneMoveTwoValue = value2;
+		playerOneMoveThreeValue = value3;
+	}	else if (player === 'Player Two') {
+		playerTwoMoveOneType = type1;
+		playerTwoMoveTwoType = type2;
+		playerTwoMoveThreeType = type3;
+		playerTwoMoveOneValue = value1;
+		playerTwoMoveTwoValue = value2;
+		playerTwoMoveThreeValue = value3;
+	}
+};
+
