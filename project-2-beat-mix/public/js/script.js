c96375b5422a2f62dfe89001246754dce020525e
Paul <sesshoumaru404@outlook.com>
2017-10-10 16:46:08 -0400

    Changes to be committed: 	modified:   .gitignore 	modified:   project-2-beat-mix/public/js/script.js

--- project-2-beat-mix/public/js/script.js
+++ project-2-beat-mix/public/js/script.js
@@ -1,6 +1,24 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = [];
+let snares = [];
+let hiHats = [];
+let rideCymbals = [];
 
+for(i=0;i<16;i++){
+	kicks[i] = false;
+	snares[i] = false;
+	hiHats[i] = false;
+	rideCymbals[i] = false;
+}
+
+const toggleDrum = (type, index) => {
+	if (!type || !index || index >= type.length || index < 0 ){
+		return;
+	}
+
+	if (type[index]) {
+		type[index] = false;
+	} else {
+		type[index] = true;
+	}
+};
