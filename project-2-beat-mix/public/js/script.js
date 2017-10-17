b0c55c316fc7512b7949645180d24baa2c52a763
Paul <sesshoumaru404@outlook.com>
2017-10-16 23:00:12 -0400

    Changes to be committed: 	modified:   script.js

--- project-2-beat-mix/public/js/script.js
+++ project-2-beat-mix/public/js/script.js
@@ -1,8 +1,8 @@
 // Drum Arrays
-let kicks = [];
-let snares = [];
-let hiHats = [];
-let rideCymbals = [];
+const kicks = [];
+const snares = [];
+const hiHats = [];
+const rideCymbals = [];
 
 for(i=0;i<16;i++){
 	kicks[i] = false;
@@ -12,13 +12,37 @@ for(i=0;i<16;i++){
 }
 
 const toggleDrum = (type, index) => {
-	if (!type || !index || index >= type.length || index < 0 ){
-		return;
+	if (!validIndex(index)) {return;}
+	if (!validArray(type)) {return;}
+
+	if (type[index] === true) { return type[index] = false;} 
+
+	return type[index] = true;
+	
+};
+
+const clear = (array) => {
+	if (!validArray(array)) {return;}
+	for(i=0;i<16;i++){
+		array[i] = false;
 	}
 
-	if (type[index]) {
-		type[index] = false;
-	} else {
-		type[index] = true;
-	}
-};
+}
+
+const validIndex = (index) => {
+	if (
+	    index < 16	&&
+	    index >= 0	
+	) {return true;}
+	return false;
+
+}
+
+const validArray = (array) => {
+	if (array === kicks ) {return true;}
+	if (array === snares ) {return true;}
+	if (array === hiHats ) {return true;}
+	if (array === rideCymbals) {return true;}
+	return false;
+
+}
